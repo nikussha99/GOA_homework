95531bc95cdad06225ad1229313cefeb175e24ef
nikussha99 <nikaiqidanaqet@gmail.com>
2024-02-27 19:33:36 +0400

    fourth commit

---
+++ day4/day4.py
@@ -0,0 +1,12 @@
+#davaleba2
+num1=int(input("your num?: "))
+num2=int(input("your num?: "))
+print(num1 + num2)
+
+
+#davaleba3
+name=input("your name?: ")
+surname=input("your surname?: ")
+age=input("your age?: ")
+info=input("droshi mogzauroba rom shegezlos romel welshi gadaxvidodi?")
+print("gamarjoba mevar " + name +" " + surname+ " mevar "+ age+" wlis "  +" droshi mogzauroba rom shemedzlos gadavidodi "+info)
