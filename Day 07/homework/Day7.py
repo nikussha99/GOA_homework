afd2195dc4afc236fa4000c26d6630b60ce7792b
nikussha99 <nikaiqidanaqet@gmail.com>
2024-03-18 20:50:00 +0400

    commit

---
+++ Day 07/homework/Day7.py
@@ -0,0 +1,48 @@
+"""
+შექმენით ალგორითმი, რომელიც მომხმარებელს შეეკითხება
+ ტემპერატურას ცელსიუსში და გადაიყვანს ფარენჰეიტში. ((variable * 1.8) + 32)
+
+"""
+
+celsius = int(input("please enter a number: "))
+farenhait=((celsius * 1.8) + 32)
+
+print(farenhait)
+
+"""
+
+მომხმარებელს შეეკითხეთ ასაკი და მოახდინეთ შედარება ისე,
+რომ ასაკი ნაკლები იყოს ოცზე და მეტი იყოს თორმეტზე.
+
+"""
+user_input = int(input("how old are you: "))
+print(user_input<20 and user_input>12)
+
+"""
+მომხმარებელს შემოატანინეთ რიცხვი და დაბეჭდეთ
+ არის თუ არა ის 100-ზე მეტი და 9-ის ჯერადი (9-ზე სრულად იყოფა).
+
+"""
+num1 = int(input("please enter your number: "))
+print(num1 <= 100 and num1/9)
+
+"""
+
+მომხმარებელს შემოატანინეთ მართკუთხედის
+ c(retangle) გვერდები (გაითვალისწინეთ, რომ შესაძლოა რიცხვი ათწილადიც იყოს) და დაუბეჭდეთ მისი ფართობი.
+
+"""
+num5 = int(input("A-please enter the length of the rectangle: " ) )
+num6 = int(input("B-please enter the width of the rectangle " ) )
+print(num5 * num6 )
+
+"""
+ივარჯიშეთ And და Or ოპერატორებზე და თქვენით ჩამოწერეთ ყველა შესაძლო ვარიანტი მათზე მუშაობისას.
+
+"""
+#and
+print(True and True)
+print(True and False)
+#or
+print(False and True)
+print(True and False)
