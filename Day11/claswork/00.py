5d20d14a873bf5d20407a184dedda744b0cb648d
nikussha99 <nikaiqidanaqet@gmail.com>
2024-04-15 00:14:58 +0400

    commit

---
+++ Day11/claswork/00.py
@@ -0,0 +1,23 @@
+num1=int(input("please enter number1: "))
+num2=int(input("please enter number2: "))
+num3=int(input("please enter number3: "))
+
+
+
+operation = input("Please enter (+, -, * or /): ")
+
+if operation == "+":
+    print(num1 + num2)
+elif operation == "-":
+    print(num1 - num3)
+elif operation == "*":
+    print(num1 * num2)
+elif operation == "/":
+    print(num1 / num3)
+
+
+
+
+
+
+
